b089933ad7aa59e07e43731c76d9538014f0be15
Michael Milton <ttmigueltt@gmail.com>
2020-01-08 23:47:57 +1100

    Fixed year detection. Restructured render() function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Client, UserHandler } from 'spotify-sdk';
 import { useLocation } from 'react-router-dom';
 import qs from 'qs';
+import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -21,6 +22,14 @@ import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import uniqBy from 'lodash.uniqby';
 
+// Set Spotify API settings
+let client = Client.instance;
+client.settings = {
+    clientId: '9c91bacd3cc149c4ac198f88b2468719',
+    scopes: ['user-top-read'],
+    redirect_uri: window.location.origin + window.location.pathname,
+};
+
 /**
  * Gets the start and end dates for eligible songs in that year. e.g. for 2019 this would be
  * 1 December 2018 and 30 November 2019
@@ -31,6 +40,28 @@ function eligibilityPeriod(year) {
         new Date(year, 10), // November of the current year
     ];
 }
+
+/**
+ * Calculates the next Hottest 100 year based on the date
+ * @param date
+ */
+function upcomingYear(date) {
+    const votingCloses = new Date(
+        date.getFullYear(),
+        0,
+        28,
+        3,
+    );
+
+    if (date < votingCloses) {
+        // If we're before the Hottest 100 date, the current year is the upcoming year
+        return date.getFullYear() - 1;
+    } else {
+        // If we're after the Hottest 100 date, next year is the upcoming year
+        return date.getFullYear();
+    }
+}
+
 
 /**
  * Returns true if a track is eligible for the given year of hottest 100
@@ -46,20 +77,30 @@ function App() {
     const hash = useMemo(() => {
         return qs.parse(location.hash.substring(1));
     }, [location.hash]);
-    const currentYear = new Date().getFullYear();
+    client.token = hash.access_token;
     const [uniqueArtists, setUniqueArtists] = useState(false);
     const [loginUrl, setLoginUrl] = useState(null);
-    const [hasRetrieved, setHasRetrieved] = useState(false);
+    const [retrievalState, setRetrievalState] = useState('notRequested');
     const [shortTerm, setShortTerm] = useState([]);
     const [mediumTerm, setMediumTerm] = useState([]);
     const [longTerm, setLongTerm] = useState([]);
+    const currentYear = upcomingYear(new Date());
     const [year, setYear] = useState(currentYear);
+
+    // If we don't have a login url, request it
+    useEffect(() => {
+        if (!('access_token' in hash)) {
+            client.login().then((url) => {
+                setLoginUrl(url);
+            });
+        }
+    }, [hash]);
 
     // If we don't yet have the user data, request it
     useEffect(() => {
-        if (!hasRetrieved) {
+        if (retrievalState === 'notRequested' && 'access_token' in hash) {
             // Only request once
-            setHasRetrieved(true);
+            setRetrievalState('requested');
             new UserHandler().me().then(user => {
                 return Promise.all(['short_term', 'medium_term', 'long_term'].map(term => {
                     return user.top('tracks', {
@@ -68,12 +109,13 @@ function App() {
                     });
                 }));
             }).then(([shortTerm, mediumTerm, longTerm]) => {
+                setRetrievalState('retrieved');
                 setShortTerm(shortTerm);
                 setMediumTerm(mediumTerm);
                 setLongTerm(longTerm);
             });
         }
-    }, [hasRetrieved]);
+    }, [retrievalState, hash, hash.access_token]);
 
     // Filter the tracks to the top 10, choosing longer term favourites where possible
     const tracks = useMemo(() => {
@@ -95,99 +137,91 @@ function App() {
 
     }, [shortTerm, mediumTerm, longTerm, year, uniqueArtists]);
 
-    // Set Spotify API settings
-    let client = Client.instance;
-    client.settings = {
-        clientId: '9c91bacd3cc149c4ac198f88b2468719',
-        scopes: ['user-top-read'],
-        redirect_uri: window.location.origin + window.location.pathname,
-    };
-
     let content;
-    if (!('access_token' in hash)) {
-        client.login().then((url) => {
-            setLoginUrl(url);
-        });
-        content = <Button variant={'contained'} size={'large'} color={'primary'}
-            href={loginUrl}>Login</Button>;
-    } else {
-        client.token = hash.access_token;
-        if (tracks.length > 0) {
-            content = <Paper>
-                <Grid container justify={'center'}>
-                    <Grid item>
-                        <FormGroup row>
-                            <TextField
-                                label="Year"
-                                type="number"
-                                value={year}
+    if (retrievalState === 'retrieved') {
+        content = <Paper>
+            <Grid container justify={'center'}>
+                <Grid item>
+                    <FormGroup row>
+                        <TextField
+                            label="Year"
+                            type="number"
+                            value={year}
+                            onChange={e => {
+                                setYear(e.target.value);
+                            }}
+                        />
+                        <FormControlLabel control={
+                            <Checkbox
+                                checked={uniqueArtists}
                                 onChange={e => {
-                                    setYear(e.target.value);
+                                    setUniqueArtists(e.target.checked);
                                 }}
                             />
-                            <FormControlLabel control={
-                                <Checkbox
-                                    checked={uniqueArtists}
-                                    onChange={e => {
-                                        setUniqueArtists(e.target.checked);
-                                    }}
-                                />
-                            }
-                                label={'Unique Artists'}
-                            />
-                        </FormGroup>
-                    </Grid>
+                        }
+                            label={'Unique Artists'}
+                        />
+                    </FormGroup>
                 </Grid>
-                <TableContainer component={Paper}>
-                    <Table style={{
-                        width: '100%',
-                    }}>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Rank</TableCell>
-                                <TableCell>Song</TableCell>
-                                <TableCell>Artists</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {
-                                tracks
-                                    .map((track, i) => {
-                                        return (
-                                            <TableRow>
-                                                <TableCell>{i + 1}</TableCell>
-                                                <TableCell>
-                                                    <Link href={track.uri}>
-                                                        {track.name}
-                                                    </Link>
-                                                </TableCell>
-                                                <TableCell>{
-                                                    track.artists.map(artist => artist.name).join(', ')
-                                                }
-                                                </TableCell>
-                                            </TableRow>
-                                        );
-                                    })
-                            }
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </Paper>;
-        } else {
-            content = (<CircularProgress/>);
-        }
+            </Grid>
+            <TableContainer component={Paper}>
+                <Table style={{
+                    width: '100%',
+                }}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Rank</TableCell>
+                            <TableCell>Song</TableCell>
+                            <TableCell>Artists</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            tracks
+                                .map((track, i) => {
+                                    return (
+                                        <TableRow>
+                                            <TableCell>{i + 1}</TableCell>
+                                            <TableCell>
+                                                <Link href={track.uri}>
+                                                    {track.name}
+                                                </Link>
+                                            </TableCell>
+                                            <TableCell>{
+                                                track.artists.map(artist => artist.name).join(', ')
+                                            }
+                                            </TableCell>
+                                        </TableRow>
+                                    );
+                                })
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Paper>;
+    } else if (!('access_token' in hash)) {
+        content = <Button
+            variant={'contained'}
+            size={'large'}
+            color={'primary'}
+            href={loginUrl}
+        >
+            Login
+        </Button>;
+    } else {
+        content = (<CircularProgress/>);
     }
 
-    return <Grid container justify={'center'}>
-        <Grid item style={{
-            maxWidth: '800px',
-        }}>
-            <Typography variant={'h1'}>
-                Spotify Hottest 100 Calculator
-            </Typography>
-            {content}
+    return <Container maxWidth={'md'}>
+        <Grid container justify={'center'}>
+            <Grid item>
+                <Typography variant={'h2'}>
+                    Spotify Hottest 100 Calculator
+                </Typography>
+                {content}
+            </Grid>
         </Grid>
-    </Grid>;
+    </Container>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,8 +6,14 @@ import * as serviceWorker from './serviceWorker';
 import {
     BrowserRouter as Router,
 } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-ReactDOM.render(<Router><App/></Router>, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <CssBaseline>
+            <App/>
+        </CssBaseline>
+    </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
