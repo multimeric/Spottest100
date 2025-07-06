ecd7bac1a74da93aef5887463131410b37e58217
Michael Milton <michael.r.milton@gmail.com>
2025-07-07 00:22:12 +1000

    Refactor countdowns into classes, add 2024 back

--- src/App.tsx
+++ src/App.tsx
@@ -5,58 +5,17 @@ import { rerankTracks, SimpleTrack } from './simpleTrack';
 import { AppBar, Box, Button, FormControl, Grid2, InputLabel, MenuItem, Paper, Select, Toolbar, Typography } from '@mui/material';
 import About from './about';
 import { TrackGrid } from './trackGrid';
-import { ArtistLimit } from './artistLimit';
 import { FieldValues, useForm, UseFormProps, UseFormReturn } from 'react-hook-form';
 import { byArtistMaxSongs, byVotingList } from './filters';
 import { useVotingList, VotingList } from './votingList';
 import { useInfiniteScroll } from './useInfiniteScroll';
-
-type SettingsProps<T extends FieldValues> = {
-  form: UseFormReturn<T>
-}
-
-type Countdown<T extends FieldValues> = {
-  name: string;
-  // component: React.FC<{ favourites: SimpleTrack[] }>;
-  // Component for requesting settinsg
-  settings: React.FC<SettingsProps<T>>
-  // Function to filter tracks based on settings
-  filter: (tracks: SimpleTrack[], settings: T, votingList: VotingList | null) => SimpleTrack[]
-  // 
-  votingListUrl: string,
-  // Props passed into useForm(), such as default values
-  formProps: UseFormProps
-}
-
-const countdowns: Record<string, Countdown<any>> = {
-  "2025-Australian": {
-    name: "Hottest 100 of Australian Songs",
-    votingListUrl: "2025_australian.json",
-    settings: ({ form }) => {
-      return <ArtistLimit control={form.control} />
-    },
-    filter: (tracks: SimpleTrack[], settings: any, votingList: VotingList | null) => {
-      // Apply the voting list filter first, since it is the most restrictive
-      tracks = byVotingList(tracks, votingList);
-      return byArtistMaxSongs(tracks, settings.limitPerArtist)
-    },
-    formProps: {
-      defaultValues: { limitPerArtist: Infinity }
-    }
-  },
-  // "2024": {
-  //   name: "Hottest 100 of 2024",
-  //   component: Countdown2024
-  // }
-}
-
-type CountDownId = keyof typeof countdowns;
+import { countdowns } from './countdown';
 
 
 export default function App() {
   const location = window.location;
 
-  const [countdownId, setCountdownId] = useState<CountDownId>("2025-Australian");
+  const [countdownId, setCountdownId] = useState("2025-Australian");
   const homeUrl = location.origin + location.pathname;
 
   const [showAbout, setShowAbout] = useState(false);
@@ -129,7 +88,9 @@ export default function App() {
             label="Time Range"
             variant="filled"
             onChange={(e) => {
-              setCountdownId(e.target.value as CountDownId);
+              const newCountdownId = e.target.value;
+              setCountdownId(newCountdownId);
+              form.reset(countdowns[newCountdownId].formProps.defaultValues);
             }}
           >
             {Object.entries(countdowns).map(([id, countdown]) => (
---
+++ src/countdown.tsx
@@ -0,0 +1,106 @@
+import { Controller, FieldValues, UseFormProps, UseFormReturn } from "react-hook-form";
+import { byVotingList, byArtistMaxSongs, byYear } from "./filters";
+import { SimpleTrack } from "./simpleTrack";
+import { VotingList } from "./votingList";
+import { ArtistLimit } from './artistLimit';
+import { TextField, Stack, Typography, Switch } from "@mui/material";
+
+type SettingsProps<T extends FieldValues> = {
+  form: UseFormReturn<T>
+}
+
+abstract class Countdown<T extends FieldValues> {
+  constructor(
+    public readonly name: string,
+    public readonly formProps: UseFormProps<T>,
+    public readonly votingListUrl: string
+  ) { }
+  // name: string;
+  // formProps: UseFormProps
+  // votingListUrl: string;
+  abstract settings(props: SettingsProps<T>): React.ReactElement;
+  filter(tracks: SimpleTrack[], settings: T, votingList: VotingList | null): SimpleTrack[] {
+    return byVotingList(tracks, votingList)
+  }
+
+  async getVotingList(): Promise<VotingList> {
+    const res = await fetch("2025_australian.json");
+    const parsed = await res.json();
+    return new VotingList(parsed);
+  }
+}
+
+type Settings2025Australian = {
+  limitPerArtist: number
+}
+
+export class Countdown2025Australian extends Countdown<Settings2025Australian> {
+  constructor() {
+    super("Hottest 100 of Australian Songs", {
+      defaultValues: { limitPerArtist: Infinity }
+    }, "2025_australian.json");
+  }
+
+  settings({ form }: SettingsProps<any>): React.ReactElement {
+    return <ArtistLimit control={form.control} />
+  }
+
+  filter(tracks: SimpleTrack[], settings: Settings2025Australian, votingList: VotingList | null): SimpleTrack[] {
+    return byArtistMaxSongs(super.filter(tracks, settings, votingList), settings.limitPerArtist);
+  }
+}
+
+type Settings2024 = {
+  limitPerArtist: number
+  useVotingList: boolean
+}
+
+export class Countdown2024 extends Countdown<Settings2024> {
+  constructor() {
+    super("Hottest 100 of 2024", {
+      defaultValues: { limitPerArtist: Infinity, useVotingList: true }
+    }, "2024.json");
+  }
+
+  settings({ form }: SettingsProps<any>): React.ReactElement {
+    return <>
+      <ArtistLimit control={form.control} />
+      <TextField
+        variant="outlined"
+        label="Eligibility"
+        multiline
+        slotProps={{
+          inputLabel: { shrink: true },
+          input: {
+            inputComponent: () => <Stack direction="row" spacing={2} alignItems="center" sx={{ padding: '8px' }}>
+              <Typography>Release Year</Typography>
+              <Controller
+                name="useVotingList"
+                control={form.control}
+                render={({ field }) => <Switch checked={field.value} onChange={field.onChange} onBlur={field.onBlur} name={field.name} inputRef={field.ref} />}
+              />
+              <Typography>Voting List</Typography>
+            </Stack>
+          }
+        }}
+      />
+    </>
+  }
+
+  filter(tracks: SimpleTrack[], settings: Settings2024, votingList: VotingList | null): SimpleTrack[] {
+    // Toggle between filtering by voting list or by year based on user settings
+    if (settings.useVotingList) {
+      tracks = byVotingList(tracks, votingList);
+    } else {
+      tracks = byYear(tracks, 2024);
+    }
+    return byArtistMaxSongs(tracks, settings.limitPerArtist);
+  }
+}
+
+export const countdowns: Record<string, Countdown<any>> = {
+  "2025-Australian": new Countdown2025Australian(),
+  "2024": new Countdown2024()
+}
+
+type CountDownId = keyof typeof countdowns;
